d9e0cb53594da034dd0383a1191aee45012e3b55
Roopab1121 <37044208+Roopab1121@users.noreply.github.com>
2019-07-07 14:06:34 +0530

    Add files via upload

--- pro.1.py
+++ pro.1.py
@@ -1,20 +1,28 @@
-def cm(m):
-    
-  if not m: return ''
-  s1 = min(m)
-  s2 = max(m)
-  for i, c in enumerate(s1):
-    if c != s2[i]:
-      return s1[:i]
-  return s1
-def main():
-  a=int(input())
-  asd=[]
-  for i in range(0,a):
-    ele=input()
-    asd.append(ele)
-  str1=cm(asd) 
-  print(str1)
-  
-if __name__== "__main__":
-  main()
+
+def get_comon_prefix_len(len,list1,list2):
+    common_len = 0
+    for k in range(len):
+        if list1[k]==list2[k]:
+            common_len+=1
+        else:
+            break
+    return common_len
+
+i = int(input())
+l,cl = [],[]
+for i in range(i):
+    l.append(list(input()))
+# print(len(l))
+for i in range(len(l)):
+    if i != len(l)-1:   
+        # print("i count:",i)
+        list1,list2 = l[i],l[i+1]
+        list1_len , list2_len = len(list1),len(list2)
+        if list1_len < list2_len:
+            common_len = get_comon_prefix_len(list1_len,list1,list2)
+        else:
+            common_len = get_comon_prefix_len(list2_len,list1,list2)
+        # print("common len",common_len," for list ",list1,list2)
+        cl.append(common_len)
+        # print("#"*30)
+print("".join(list1[:common_len]))
